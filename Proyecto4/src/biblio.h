e2aa978becd6ae4ce11839b2ab0eae2633afe276
unknown <walter.ezequiel.reyes@gmail.com>
2022-05-17 00:42:13 -0300

    proyect 4/5

---
+++ Proyecto4/src/biblio.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * biblio.h
+ *
+ *  Created on: 17 may. 2022
+ *      Author: casa
+ */
+
+#ifndef BIBLIO_H_
+#define BIBLIO_H_
+
+
+
+#endif /* BIBLIO_H_ */
---
+++ Proyecto5/src/biblio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblio.h"
+/*
+ * biblio.c
+ *
+ *  Created on: 17 may. 2022
+ *      Author: casa
+ */
+
+
