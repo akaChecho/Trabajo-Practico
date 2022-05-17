8435861143dd64155d3d9480dcc95eef04c89991
unknown <walter.ezequiel.reyes@gmail.com>
2022-05-17 00:03:37 -0300

    subir proyecto 2

---
+++ Proyecto2/src/Proyecto2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Proyecto2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World2!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
